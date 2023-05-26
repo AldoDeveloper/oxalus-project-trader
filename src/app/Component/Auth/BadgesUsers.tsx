006864e2705cdcc5e947dcb19e791d34e8568279
Aldo Ratmawan <aldo.ratmawan9999@gmail.com>
2023-05-26 17:06:19 +0700

    new Branch

--- src/app/Component/Auth/BadgesUsers.tsx
+++ src/app/Component/Auth/BadgesUsers.tsx
@@ -1,6 +1,7 @@
 import { LoaderPropsAuth } from "../../interface/Props";
 import imgs from '../../../assets/man.png';
 import { NavLink } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
 
 interface PropsProfiles{
     usersAuth: LoaderPropsAuth,
@@ -9,9 +10,19 @@ interface PropsProfiles{
 export default function BadgesUsers(props: PropsProfiles){
     return(
        <>
-            <NavLink to={'/dasboard/profile'} className='text-decoration-none text-white d-flex gap-3'>
+            <NavLink to={'#'} className='text-decoration-none text-white d-flex gap-3'>
                 <span className="align-self-center">{ props?.usersAuth?.users?.data.email }</span>
-                <img src={imgs} width={'35px'} className="rounded-circle" alt="" />
+                <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic" style={{backgroundColor: 'transparent', border: 'none', padding: '0px'}}>
+                    <img src={imgs} id="dropdown-basic" width={'35px'} className="rounded-circle" alt="" />
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <NavLink to={'/dasboard/profile'} className={'dropdown-item'}>PROFILE</NavLink>
+                    <Dropdown.Item href="#/action-2">LOGOUT</Dropdown.Item>
+                    <Dropdown.Divider/>
+                    <Dropdown.Item href="#/action-3">PAYMENT</Dropdown.Item>
+                </Dropdown.Menu>
+                </Dropdown>
             </NavLink>
        </>
     )
--- src/app/Dasboard/LayoutDasboard.tsx
+++ src/app/Dasboard/LayoutDasboard.tsx
@@ -2,11 +2,19 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 
 export function LayoutDasboard(){
+    
     return(
         <>
-            <div className="d-flex gap-3">
-                <Sidebar/>
-                <Outlet/>
+            <div className="container-fluid">
+                <div className="row g-3">
+                    <div className="col col-lg-2 col-sm-4" style={{overflowY: 'scroll'}}>
+                        <Sidebar/>
+                    </div>
+                    <div className="col col-lg-10 col-sm-9 justify-content-start"
+                     style={{overflowY: 'scroll' }}>
+                        <Outlet/>
+                    </div>
+                </div>
             </div>
         </>
     )
